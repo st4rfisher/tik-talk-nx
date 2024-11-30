d2b203f2680d49033b8b96c5cd682dd0b0ec761f
starfisher <supremeeworker@gmail.com>
2024-11-30 18:05:13 +0300

    добавление поиска

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,24 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProfileService } from '@/services/profile.service';
 import { Profile } from '@/model/profile';
 import { ProfileCardComponent } from '@/components/profile-card/profile-card.component';
+import { ProfileFiltersComponent } from "./profile-filters/profile-filters.component";
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
     ProfileCardComponent,
-  ],
+    ProfileFiltersComponent
+],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
-  profiles: Profile[] = []
 
-  constructor(private profileService: ProfileService) {
-    this.profileService.getTestAccounts().subscribe((response: any) => {
-      this.profiles = response
-      // console.log(response)
-    })
-  }
+export class SearchComponent {
+  profileService = inject(ProfileService)
+  profiles = this.profileService.filteredProfiles
 }
