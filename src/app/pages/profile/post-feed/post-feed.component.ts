e03e434153a27b0613366d3f300bfc7f96754234
starfisher <supremeeworker@gmail.com>
2024-12-15 12:24:26 +0300

    создание компонента фида с постами

--- src/app/pages/profile/post-feed/post-feed.component.ts
+++ src/app/pages/profile/post-feed/post-feed.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2, inject } from '@angular/core';
 import { PostInputComponent } from "../post-input/post-input.component";
 import { PostComponent } from "../post/post.component";
+import { PostService } from '@/services/post.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-post-feed',
   standalone: true,
-  imports: [PostInputComponent, PostComponent],
+  imports: [
+    PostInputComponent,
+    PostComponent
+  ],
   templateUrl: './post-feed.component.html',
   styleUrl: './post-feed.component.scss'
 })
 
 export class PostFeedComponent {
+  postService = inject(PostService)
+  posts = this.postService.posts
+  hostElement = inject(ElementRef)
+  renderer = inject(Renderer2) //прослойка для взаимодействия с элементами на разных платформах
 
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.resizeFeed()
+    console.log(1)
+  }
+
+  constructor() {
+    firstValueFrom(this.postService.fetchPosts())
+  }
+
+  ngAfterViewInit(): void {
+    this.resizeFeed()
+  }
+
+  resizeFeed() {
+    const { top } = this.hostElement.nativeElement.getBoundingClientRect()
+    const height = window.innerHeight - top - 24
+    this.renderer.setStyle(this.hostElement.nativeElement, 'height', `${height}px`)
+  }
 }
