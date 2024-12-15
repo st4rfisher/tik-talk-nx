792fc1c3103ae9db5cd25587fb879bd28111b085
starfisher <supremeeworker@gmail.com>
2024-12-15 12:15:24 +0300

    рефакторинг компонента с хедером в профиле

--- src/app/components/profile-header/profile-header.component.ts
+++ src/app/components/profile-header/profile-header.component.ts
@@ -1,13 +1,14 @@
 import { Profile } from '@/model/profile';
 import { Component, input } from '@angular/core';
 import { ImageUrlPipe } from '@/helpers/pipes/image-url.pipe';
+import { AvatarComponent } from "../../common-ui/avatar/avatar.component";
 
 @Component({
   selector: 'app-profile-header',
   standalone: true,
   imports: [
-    ImageUrlPipe
-  ],
+    AvatarComponent
+],
   templateUrl: './profile-header.component.html',
   styleUrl: './profile-header.component.scss'
 })
