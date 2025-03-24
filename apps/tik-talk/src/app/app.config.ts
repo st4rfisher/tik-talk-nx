99c017e2c30793c1341bc7a2e34eb8257bc65c8f
starfisher <supremeeworker@gmail.com>
2025-03-24 20:55:45 +0300

    добавление ngrx

--- apps/tik-talk/src/app/app.config.ts
+++ apps/tik-talk/src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authTokenInterceptor } from '@tt/auth';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authTokenInterceptor])),
+    provideStore(),
+    provideEffects()
   ],
 };
