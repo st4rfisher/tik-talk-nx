1722c6cf2fc0ca250703cbf264f5b38331312d97
starfisher <supremeeworker@gmail.com>
2025-04-02 18:16:11 +0300

    рефакторинг post

--- libs/posts/src/lib/feature-posts-wall/post/post.component.ts
+++ libs/posts/src/lib/feature-posts-wall/post/post.component.ts
@@ -11,9 +11,9 @@ import { firstValueFrom } from 'rxjs';
 import { AvatarComponent } from '@tt/common-ui';
 import { DatePipe } from '@angular/common';
 import { IconComponent } from '@tt/common-ui';
-import { Post, PostComment, PostService } from '../../data';
+import { Post, PostComment, PostService, postsActions, selectComments } from '../../data';
 import { CommentComponent, PostInputComponent } from '../../ui';
-
+import { Store } from '@ngrx/store';
 // import { MessageInputComponent } from '../../../common-ui/message-input/message-input.component';
 
 @Component({
@@ -30,35 +30,25 @@ import { CommentComponent, PostInputComponent } from '../../ui';
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
+
 export class PostComponent implements OnInit {
-  postService = inject(PostService);
+  // store = inject(Store);
   post = input<Post>();
   comments = signal<PostComment[]>([]);
-  initialPartOfComments = signal<PostComment[]>([]);
-  startValue = signal<number>(5);
-  endValue = signal<number>(10);
 
   async onCreated() {
-    const comments = await firstValueFrom(
-      this.postService.getCommentsByPostId(this.post()!.id)
-    );
-    this.comments.set(comments);
-  }
-
-  showMoreComments() {
-    const nextPartOfComments = this.comments().slice(
-      this.startValue(),
-      this.endValue()
-    );
-    console.log(nextPartOfComments);
-    this.initialPartOfComments.set(nextPartOfComments);
-    this.startValue.set(this.startValue() + 5);
-    this.endValue.set(this.endValue() + 5);
+    // this.store.dispatch(postsActions.fetchComments({ postId: this.post()?.id as number }))
+    // const comments = await firstValueFrom(
+    //   this.postService.getCommentsByPostId(this.post()!.id)
+    // );
+    // this.comments = this.store.selectSignal(selectComments)
+    // this.comments.set(comments) ;
   }
 
   async ngOnInit() {
+    // console.log(this.post())
     this.comments.set(this.post()!.comments);
-    console.log(this.comments());
+    // console.log(this.comments());
     // this.initialPartOfComments.set(this.comments().slice(0, 5))
     // console.log(this.comments.length)
   }
