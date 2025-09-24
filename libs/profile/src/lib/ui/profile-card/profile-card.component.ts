661b4905f8a0e9061a835476374096ffffc60ec5
starfisher <supremeeworker@gmail.com>
2025-09-24 16:50:14 +0300

    добавление перехода на страницу профиля по клику на имя пользователя на странице поиска

--- libs/profile/src/lib/ui/profile-card/profile-card.component.ts
+++ libs/profile/src/lib/ui/profile-card/profile-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '@tt/interfaces/profile';
 import { ImageUrlPipe } from '@tt/common-ui';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [ImageUrlPipe],
+  imports: [
+    ImageUrlPipe,
+    RouterLink
+  ],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
