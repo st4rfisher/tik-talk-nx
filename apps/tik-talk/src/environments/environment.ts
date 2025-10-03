62f8521f9fafd28421558cff91b4cf23c92e1b85
starfisher <supremeeworker@gmail.com>
2025-10-03 16:32:15 +0300

    добавление корректной ссылки на API

--- apps/tik-talk/src/environments/environment.ts
+++ apps/tik-talk/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://icherniakov.ru/yt-course'
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://icherniakov.ru/yt-course'
 };
